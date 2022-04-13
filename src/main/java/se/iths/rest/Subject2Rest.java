b7aa9d6eef621b06d66493f2fa4838a2391a5c11
Tahgue <taharguemir@gmail.com>
2022-04-13 11:24:04 +0200

    fix subject

--- src/main/java/se/iths/rest/Subject2Rest.java
+++ src/main/java/se/iths/rest/Subject2Rest.java
@@ -1,7 +1,6 @@
 package se.iths.rest;
 
 import se.iths.entity.Subject;
-import se.iths.entity.Teacher;
 import se.iths.service.StudentService;
 import se.iths.service.SubjectService;
 import se.iths.util.Err;
@@ -26,66 +25,44 @@ public class   Subject2Rest {
     }
 
     @GET
-    public Response getAllSubjects() {
-        List<Subject> foundSubject = subjectService.getAllSubjects();
-        if (foundSubject == null) {
-
-            throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND)
-                    .entity("The list of Subject is empty").build());
-        }
-        return  Response.ok(foundSubject).build();
-
+    public List<Subject> getAll() {
+        return subjectService.getAllSubjects();
     }
 
     @GET
     @Path("/{id}")
-    public Response getSubject(@PathParam("id") Long id)  {
-        Subject subject = subjectService.getSubject(id);
-        if (subject == null) {
-            Err err = new Err ("No Subject with id " + id + " found");
-            return  Response
-                    .status(Response.Status.BAD_REQUEST)
-                    .entity(err)
-                    .build();
-        }
-        return Response .status(Response.Status.OK)
-                .entity(subject)
-                .build();
-
-
+    public Subject getById (@PathParam("id") Long id) {
+        return subjectService.getSubject(id);
     }
 
     @POST
-    public Response addSubject(Subject subject) {
+    public Response add(Subject subject) {
         try {
             subjectService.addSubject(subject);
-            return Response.status(Response.Status.CREATED).build();
+            return Response.status(Response.Status.CREATED).entity(subject).build();
         } catch (ConstraintViolationException e) {
-            return Response.status(Response.Status.BAD_REQUEST).entity(e.getLocalizedMessage()).build();
+            return Response.status(Response.Status.BAD_REQUEST).entity("ConstraintViolationException" ).build();
         }
     }
 
     @PUT
-    @Path("/{id}")
-    public Response updateSubject(@PathParam("id") Long id,Subject subject) {
+    public Response update(Subject subject) {
         try {
-            subjectService.update(id, subject);
-            return Response.status(Response.Status.OK).build();
-        } catch (Exception error){
-            throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND)
-                    .entity  ("No Subject with id "  + id +  " found " ).build());
+            subjectService.update(subject);
+            return Response.status(Response.Status.OK).entity(subject).build();
+        } catch (ConstraintViolationException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("ConstraintViolationException" ).build();
         }
     }
 
     @DELETE
     @Path("/{id}")
-    public Response deleteSubject(@PathParam("id") Long id) {
+    public Response delete(@PathParam("id") Long id) {
         try {
             subjectService.delete(id);
             return Response.status(Response.Status.OK).build();
-        } catch (Exception error){
-            throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND)
-                    .entity  ("No Subject with id "  + id +  " found " ).build());
+        } catch (ConstraintViolationException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("No Subject with id" + id + " found ").build();
         }
     }
 }
